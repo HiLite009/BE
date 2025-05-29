0dd913f4656e7106da82c03d879c6148e2379fc8
easyhak <jooin2000@cau.ac.kr>
2025-05-29 18:04:21 +0900

    Feat: CI 설정

--- src/test/java/org/example/hilite/HiliteApplicationTests.java
+++ src/test/java/org/example/hilite/HiliteApplicationTests.java
@@ -1,11 +1,25 @@
 package org.example.hilite;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.yml")
 class HiliteApplicationTests {
 
   @Test
   void contextLoads() {}
+
+  @Value("${jwt.secret}")
+  private String jwtSecret;
+
+  @Test
+  void checkJwtSecret() {
+    System.out.println("🔥 Loaded JWT Secret from application-test.yml: " + jwtSecret);
+//    assert jwtSecret.equals("test-jwt-secret-for-testing");
+  }
 }
