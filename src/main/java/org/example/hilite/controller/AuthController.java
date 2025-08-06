ce789f2b8fb19ba971cf0e7246f4e6de99bb9ddb
easyhak <jooin2000@cau.ac.kr>
2025-08-06 19:36:22 +0900

    Revert "chore: Update AuthController to include API path prefix"
    This reverts commit cb4ae42cb071b741df3f783b6a19835241a2db89.

--- src/main/java/org/example/hilite/controller/AuthController.java
+++ src/main/java/org/example/hilite/controller/AuthController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
 @RequiredArgsConstructor
 @Slf4j
 public class AuthController {
